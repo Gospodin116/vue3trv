12573e301518f183fba679482f7d52c2a037c05f
Gospodin116 <dima9595k@mail.ru>
2021-01-14 09:09:47 +0300

    Update routes.js
    ADD: Добавлена страница Login в роутах
    DEL: Удален метод для динамической загрузки страниц, так как вызвал Warning в консоле.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-function loadComponent(component) {
-    return () => import(`./pages/${component}.vue`)
-}
+// Pages
+const Index = () => import('./pages/index.vue')
+const AuthLogin = () => import('./pages/auth/login.vue')
 
 const routes = [
-    { path: '/', name: 'index', component: loadComponent('index') }
+    { path: '/', name: 'index', component: Index },
+
+    // Auth (test page)
+    { path: '/auth/login', name: 'auth.login', component: AuthLogin },
 ]
 
 export default createRouter({
