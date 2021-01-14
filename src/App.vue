1d28ffe2ca64c65493a203935d3ee79df5078296
Gospodin116 <dima9595k@mail.ru>
2021-01-14 09:08:16 +0300

    Обнавлена навигация
    Добавлена навигация на все страницы

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <ul>
+    <li><router-link :to="{ name: 'index' }">Index</router-link></li>
+    <li><router-link :to="{ name: 'auth.login' }">Login</router-link></li>
+  </ul>
   <router-view></router-view>
 </template>
 
